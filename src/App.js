8ae13b16678c36523704ebedc37160f777d55c19
DashaKavalenka <darya.kavalenka@itechart-group.com>
2021-02-04 00:44:32 +0300

    refactor: move mock data

--- src/App.js
+++ src/App.js
@@ -4,41 +4,7 @@ import RewardsTabs from './components/RewardsTabs';
 import RewardsHeader from './components/RewardsHeader';
 import Modal from './components/Modal';
 import { distanceInWordsToNow } from './dateHelpers';
-
-const users = ['John Chen', 'David Green', 'Alex Brown', 'Rajesh Kumar'];
-
-const mock = [
-  {
-    awardedPerson: 'David Green',
-    giver: 'John Chen',
-    date: '2h ago',
-    comment:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    id: 'firstComment',
-  },
-  {
-    awardedPerson: 'Alex Brown',
-    giver: 'Rajesh Kumar',
-    date: '3h ago',
-    comment:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    id: 'secondComment',
-  },
-  {
-    awardedPerson: 'Dasha Kavalenka',
-    giver: 'Rajesh Kumar',
-    date: '4h ago',
-    comment:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    id: 'thirdComment',
-  },
-];
-
-const currentUser = {
-  userName: 'Dasha Kavalenka',
-  userRewards: 1000,
-  userGive: 100,
-};
+import { mock, currentUser, users } from './mockData';
 
 function App() {
   const [rewards, setRewards] = useState(mock);
@@ -75,7 +41,7 @@ function App() {
     <div className="App">
       <RewardsHeader currentUser={user} />
       <RewardsTabs rewards={rewards} userAwards={userAwards} />
-      <Modal users={users} addRewards={addRewards} />
+      <Modal currentUser={user} users={users} addRewards={addRewards} />
     </div>
   );
 }
