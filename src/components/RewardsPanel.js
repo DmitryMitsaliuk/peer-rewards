b96aac1b267765d4f14d65f2667721de75a558a2
DashaKavalenka <darya.kavalenka@itechart-group.com>
2021-02-03 22:23:23 +0300

    feat: add tabs panel component

---
+++ src/components/RewardsPanel.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    Divider,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Avatar,
+    makeStyles, Typography
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+    author: {
+        color: theme.palette.primary.main,
+    },
+    date: {
+        display: 'block',
+    }
+}));
+
+
+
+const RewardsPanel = ({rewards}) => {
+
+    const classes = useStyles();
+    return (
+        <div>
+            <List className={classes.root}>
+                {rewards?.map(({awardedPerson, giver, date, comment}) => (
+                    <div key={date}>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt={awardedPerson} src="../assets/avatar.png"/>
+                            </ListItemAvatar>
+                            <ListItemText
+                                className={classes.author}
+                                primary={`${awardedPerson} rewarded by ${giver}`}
+                                secondary={
+                                    <>
+                                        <Typography
+                                            component="span"
+                                            variant="body2"
+                                            color="textSecondary"
+                                            className={classes.date}
+                                        >
+                                            {date}
+                                        </Typography>
+                                        <Typography
+                                            component="span"
+                                            color="textPrimary"
+                                        >
+                                            {comment}
+                                        </Typography>
+
+                                    </>
+                                }
+                            />
+
+                        </ListItem>
+                        <Divider variant="inset" component="li"/>
+                    </div>)
+                )}
+            </List>
+        </div>
+    );
+}
+
+RewardsPanel.propTypes = {
+    rewards: PropTypes.array,
+};
+
+export default RewardsPanel;
