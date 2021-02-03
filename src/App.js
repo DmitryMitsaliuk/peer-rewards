79a95a16a3d83eec908d9896952b83a9294e2710
DashaKavalenka <darya.kavalenka@itechart-group.com>
2021-02-03 22:23:51 +0300

    feat: add modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import RewardsTabs from './components/RewardsTabs'
+import RewardsHeader from './components/RewardsHeader';
+import FormDialog from './components/Modal';
+import {distanceInWordsToNow} from './dateHelpers';
+
+
+const users = ['Dasha Kavalenka', 'John Chen', 'David Green', 'Alex Brown', 'Rajesh Kumar'];
+
+
+const mock = [{
+    awardedPerson: 'David Green',
+    giver: 'John Chen',
+    date: '2h ago',
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    id: 'firstComment'
+}, {
+    awardedPerson: 'Alex Brown',
+    giver: 'Rajesh Kumar',
+    date: '3h ago',
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    id: 'secondComment'
+
+}, {
+    awardedPerson: 'Dasha Kavalenka',
+    giver: 'Rajesh Kumar',
+    date: '4h ago',
+    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    id: 'thirdComment'
+},
+]
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [rewards, setRewards] = useState(mock);
+    const myUser = 'Dasha Kavalenka';
+
+    const userAwards = rewards.filter(({awardedPerson}) => {
+        return awardedPerson === myUser;
+    });
+
+    const addRewards = (reward) => {
+       const date = distanceInWordsToNow();
+        const newReward = {
+            ...reward,
+            date,
+            giver: myUser,
+        }
+        setRewards([...rewards, newReward])
+    }
+
+    return (
+        <div className="App">
+            <RewardsHeader userFullName={myUser}/>
+            <RewardsTabs rewards={rewards} userAwards={userAwards}/>
+            <FormDialog users={users} addRewards={addRewards}/>
+        </div>
+    );
 }
 
 export default App;
