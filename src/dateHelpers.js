e7328694dea789214d39ae9793acb8095366e8d9
DashaKavalenka <darya.kavalenka@itechart-group.com>
2021-02-03 22:24:30 +0300

    feat: add date helper

---
+++ src/dateHelpers.js
@@ -0,0 +1,6 @@
+import {formatDistance} from 'date-fns';
+
+export function distanceInWordsToNow(date) {
+    const formattedDate = date ? new Date(date) : new Date();
+    return formatDistance(formattedDate, new Date() );
+}
